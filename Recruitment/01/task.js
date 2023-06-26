6ad9335bdbfb7fcec64337a3fa4c65d160cd9871
olciaciura <olcia.ciura@gmail.com>
2023-06-26 23:10:55 +0200

    upload okey, live testing doesn't work

--- Recruitment/01/task.js
+++ Recruitment/01/task.js
@@ -1,17 +1,18 @@
-const general = {
+window.general = (function() {
 
-    /**
-    * Returns sum of a and b.
-    */
-    add: function(a, b) {
-    },
+    return{
+        /**
+        * Returns sum of a and b.
+        */
+        add: function(a, b) {
+        },
 
-    /**
-    * Returns difference of a and b.
-    */
-    subtract: function(a, b) {
-    },
-
-};
+        /**
+        * Returns difference of a and b.
+        */
+        subtract: function(a, b) {
+        },
+    };
+})();
 
 module.export = general;
